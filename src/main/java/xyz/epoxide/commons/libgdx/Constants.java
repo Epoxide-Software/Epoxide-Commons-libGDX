c03bacf1151afd6275666615fe5fb138695c082f
lclc98 <liam.c14785@gmail.com>
2016-12-20 21:00:03 +1000

    Reverted bundling Epoxide Commons

--- src/main/java/xyz/epoxide/commons/libgdx/Constants.java
+++ src/main/java/xyz/epoxide/commons/libgdx/Constants.java
@@ -3,5 +3,5 @@ package xyz.epoxide.commons.libgdx;
 public class Constants {
 
     public static final String NAME = "Epoxide Common libGDX";
-    public static final String VERSION = "0.0.0.0";
+    public static final String VERSION = "0.0.0";
 }
